00314341dffb39685490d37651746f9a828d7ea3
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-05-14 00:29:41 +0300

    responsive css classes (media query)

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 import { Link } from 'react-router-dom';
 
-const navbar = props => {
+const Navbar = props => {
+    const [showMenu, setShowMenu] = useState(false)
     return (
         <nav className="navbar" >
-            <a href="#" className="logo">logo</a>
-            <ul className="main-nav" id="js-menu">
-                <li>
-                    <Link to='/' className="nav-links">Home</Link>
+            <span className="navbar-toggle" id="js-navbar-toggle" onClick={() => setShowMenu(!showMenu)}>
+                <i className="fa fa-bars"></i>
+            </span>
+            <Link to="/" className="logo">logo</Link>
+            <ul className={showMenu ? 'active' : 'main-nav'} id="js-menu">
+                <li className="nav-links">
+                    <Link to='/' >Home</Link>
                 </li>
-                <li>
-                    <Link to='/' className="nav-links">Products</Link>
+                <li className="nav-links">
+                    <Link to='/' >Products</Link>
                 </li>
-                <li>
-                    <Link to='/' className="nav-links">About Us</Link>
+                <li className="nav-links">
+                    <Link to='/' >About Us</Link>
                 </li>
-                <li>
-                    <Link to='/' className="nav-links">Contact Us</Link>
+                <li className="nav-links">
+                    <Link to='/' >Contact Us</Link>
                 </li>
-                <li>
-                    <Link to='/' className="nav-links">Blog</Link>
+                <li className="nav-links">
+                    <Link to='/' >Blog</Link>
                 </li>
             </ul>
         </nav >
     )
 }
 
-export default navbar;
+export default Navbar;
