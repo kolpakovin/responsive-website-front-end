6b656b94a3c65082e9ba64e91e064af9245e6912
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-05-16 15:31:24 +0300

    Updates

---
+++ precache-manifest.7e83379aca4baf6e1044e62f894f045d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17fa522a978af0de394c5067ababdfb",
+    "url": "/responsive-website-front-end/index.html"
+  },
+  {
+    "revision": "df6b00786d477eede14e",
+    "url": "/responsive-website-front-end/static/css/main.eea9dc17.chunk.css"
+  },
+  {
+    "revision": "c52006e514548b63b209",
+    "url": "/responsive-website-front-end/static/js/2.ae171c3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/responsive-website-front-end/static/js/2.ae171c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6b00786d477eede14e",
+    "url": "/responsive-website-front-end/static/js/main.a90c96be.chunk.js"
+  },
+  {
+    "revision": "06d2a8a4417b8bb61a39",
+    "url": "/responsive-website-front-end/static/js/runtime-main.db5a50de.js"
+  }
+]);
